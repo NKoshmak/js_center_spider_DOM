daa5b1b66d1f1846764cff725ea3fba9f7c8e962
Koshmak Nataliia <koshmaknatali@gmail.com>
2024-08-14 18:45:36 -0600

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,7 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+spider.style.left = Math.round(wall.clientWidth / 2 - spider.offsetWidth / 2) + "px";
+spider.style.top = Math.round(wall.clientHeight / 2 - spider.offsetHeight / 2) + "px";
+
+//# sourceMappingURL=index.f75de5e1.js.map
